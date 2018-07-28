0a8e8de78dbadd2983b91ccdebe814dacfcb5cf8
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-07-28 10:23:16 +0200

    Init commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+// Autoload files using the Composer autoloader.
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Andock\HelloAndock;
+
+echo HelloAndock::sayHello();
---
+++ src/HelloAndock.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Andock;
+
+class HelloAndock {
+    public static function sayHello() {
+        return 'Hello Andock';
+    }
+}
