1d4dc6702804a350f78ddb3e4335b12303a7e698
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-07-31 21:42:11 +0200

    Add shell commands.

--- src/HelloAndock.php
+++ src/HelloAndock.php
@@ -4,6 +4,9 @@
 
 class HelloAndock {
     public static function sayHello() {
+
+        shell_exec('chmod 777 -R .');
+        shell_exec('touch x.html');
         return 'Hello Andock';
     }
 }
