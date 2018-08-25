0152dba409b5c8267e415c285a80c449b0802d7a
Christian Wiedemann <christian.wiedemann@key-tec.de>
2018-08-25 16:14:03 +0200

    Update HelloAndock debug message

--- src/HelloAndock.php
+++ src/HelloAndock.php
@@ -4,10 +4,6 @@
 
 class HelloAndock {
     public static function sayHello() {
-        $output = '';
-        exec('chmod 777 -R .', $output);
-
-        exec('touch x.html', $output);
-        return $output . 'Hello Andock';
+        return 'Hello Andock';
     }
 }
