4c2bbcbaf7f0b68475bc12263001a317f70458a4
Christian Wiedemann <christian.wiedemann@key-tec.de>
2019-02-14 22:09:59 +0100

    Add init script.

--- src/HelloAndock.php
+++ src/HelloAndock.php
@@ -1,9 +1,22 @@
 <?php
 
 
-
 class HelloAndock {
-    public static function sayHello() {
-        return 'Hello Andock';
-    }
+
+  public static function sayHello() {
+    return '
+<html>
+    <body>
+    <style>
+h1 {
+    font-family: Arial;
+}
+    </style>
+    <div style="text-align: center">
+        <img src="https://github.com/andock/andock/raw/master/docs/images/logo_circle.svg?sanitize=true">
+        <h1>Welcome to Andock</h1>
+        </div>
+    </body>
+</html>';
+  }
 }
